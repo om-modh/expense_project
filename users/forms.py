26204e858da35fa3e1d3e359125685aeaaf69eaa
Om Modh <om.modh1729@gmail.com>
2023-09-28 18:03:41 +0530

    first commit

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,8 @@
 from django import forms
-from django.contrib.auth.models import User
-from .models import User
+from .models import User, UserProfile
 from django.contrib.auth.forms import UserCreationForm
 from django.forms.widgets import NumberInput
-
+# from django.contrib.auth.models import User
 
 class UserRegistrationForm(UserCreationForm):
     first_name = forms.CharField(label='First Name', max_length=20, empty_value=False)
@@ -14,5 +13,37 @@ class UserRegistrationForm(UserCreationForm):
     PhoneNo = forms.CharField(label='Phone Number', max_length=12, min_length=9)
     
     class Meta:
-        model = User
-        fields = ['first_name', 'last_name', 'username', 'email', 'DateOfBirth', 'PhoneNo', 'password1', 'password2']
+        model = UserProfile
+        fields = ['FirstName', 'LastName', 'Username', 'Email', 'password1', 'password2']
+        widgets = {
+            'FirstName' : forms.TextInput(attrs={'class': 'top-row field-wrap'}),
+            'LastName' : forms.TextInput(attrs={'class' : 'top-row field-wrap'}),
+        }
+    
+    
+    
+    
+    
+# class UserRegistrationForm(UserCreationForm):
+#     class Meta:
+#         model = UserProfile
+#         fields = ['FirstName', 'LastName', 'Email', 'Gender', 'BirthDate', 'PhoneNumber', 'password1', 'password2']
+#         # widgets = {
+#         #     'first_name' : forms.TextInput(attrs={'class': 'form-control'}),
+#         # }
+    
+    
+    
+    
+# GENDER_CHOICES = (
+#     (0, 'Male'),
+#     (1, 'Female'),
+#     (2, 'Not Specified'),
+# )
+# first_name = forms.CharField(label='First Name', max_length=20, empty_value=False)
+# last_name = forms.CharField(label='Last Name', max_length=20)
+# username = forms.CharField(label='Username', max_length=30)
+# Gender = forms.ChoiceField()
+# email = forms.EmailField()
+# DateOfBirth = forms.DateField(label='Birth Date', widget=NumberInput(attrs={'type':'date'}))
+# PhoneNo = forms.CharField(label='Phone Number', max_length=12, min_length=9)
--- users/models.py
+++ users/models.py
@@ -1,13 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, AbstractBaseUser
+from phone_field import PhoneField
 
+
+GENDER_CHOICES = (
+    (0, 'Male'),
+    (1, 'Female'),
+    (2, 'Not Specified')
+)
 class UserDetail(models.Model):
     Users = models.OneToOneField(User, on_delete=models.CASCADE)
     FirstName = models.CharField(max_length=25, blank=False)
     LastName = models.CharField(max_length=25)
     Username = models.CharField(max_length=30, unique=True, null=False)
+    gender = models.IntegerField(choices=GENDER_CHOICES)    
     Email = models.EmailField(max_length=120, unique=True)
-    DateOfBirth = models.DateField()
+    BirthDate = models.DateField()
+    PhoneNumber = PhoneField(blank=True)
 
     def __str__(self):
         return self.Username
