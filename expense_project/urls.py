fb27f066c5f923482df853ee17073f6a6ff7a207
Om Modh <om.modh1729@gmail.com>
2023-09-28 17:56:19 +0530

    first commit

--- expense_project/urls.py
+++ expense_project/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', user_views.register.register, name = 'register'),
+    path('register/', user_views.register, name = 'register'),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('home/', manager_views.home, name = 'manager-home'),
