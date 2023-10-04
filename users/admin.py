2ca90764578326b8935093a04622ccd7a4ca0fdf
Om Modh <om.modh1729@gmail.com>
2023-10-04 19:06:04 +0530

    without frontend

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,1 @@
-from django.contrib import admin
-
-from .models import UserDetail  
-
-admin.site.register(UserDetail)
+from django.contrib import admin  
--- users/forms.py
+++ users/forms.py
@@ -1,49 +1,18 @@
 from django import forms
-from .models import User, UserDetail
+from django.forms import widgets
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.forms.widgets import NumberInput
-# from django.contrib.auth.models import User
 
-class UserRegistrationForm(forms.ModelForm):
-    # FirstName = forms.CharField(label='First Name', max_length=20, empty_value=False)
-    # LastName = forms.CharField(label='Last Name', max_length=20)
-    # username = forms.CharField(label='Username', max_length=30)
-    # DateOfBirth = forms.DateField(label='Birth Date', widget=NumberInput(attrs={'type':'date'}))
-    # PhoneNo = forms.CharField(label='Phone Number', max_length=12, min_length=9)
-    email = forms.EmailField(max_length=200, help_text="Required")
+
+class UserRegistrationForm(UserCreationForm):
+    email = forms.EmailField(max_length=200)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
-        # widgets = {
-        #     'FirstName' : forms.TextInput(attrs={'class': 'top-row field-wrap'}),
-        #     'LastName' : forms.TextInput(attrs={'class' : 'top-row field-wrap'}),
-        # }
-    
-    
-    
-    
-    
-# class UserRegistrationForm(UserCreationForm):
-#     class Meta:
-#         model = UserProfile
-#         fields = ['FirstName', 'LastName', 'Email', 'Gender', 'BirthDate', 'PhoneNumber', 'password1', 'password2']
-#         # widgets = {
-#         #     'first_name' : forms.TextInput(attrs={'class': 'form-control'}),
-#         # }
-    
-    
-    
-    
-# GENDER_CHOICES = (
-#     (0, 'Male'),
-#     (1, 'Female'),
-#     (2, 'Not Specified'),
-# )
-# first_name = forms.CharField(label='First Name', max_length=20, empty_value=False)
-# last_name = forms.CharField(label='Last Name', max_length=20)
-# username = forms.CharField(label='Username', max_length=30)
-# Gender = forms.ChoiceField()
-# email = forms.EmailField()
-# DateOfBirth = forms.DateField(label='Birth Date', widget=NumberInput(attrs={'type':'date'}))
-# PhoneNo = forms.CharField(label='Phone Number', max_length=12, min_length=9)
+        fields = ['username', 'email', 'password1', 'password2']  
+        widgets = {
+            'username' : forms.TextInput(attrs={'placeholder': 'Username'}),
+            'email' : forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'password1' : forms.PasswordInput(attrs={'placeholder':'Password'}),
+            'password2' : forms.PasswordInput(attrs={'placeholder':'Confirm Password'}),
+        }
