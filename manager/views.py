588b422cbe0f40e520116069e80d1f89ca6b3e48
Om Modh <om.modh1729@gmail.com>
2023-10-18 19:04:57 +0530

    expense, income graph...

--- manager/views.py
+++ manager/views.py
@@ -3,6 +3,7 @@ from .forms import UserExpenseForm, UserIncomeForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Expense, Income
+from datetime import datetime, timedelta
 
 @login_required
 def home(request):
@@ -18,24 +19,24 @@ def income(request):
     money = []
     user = request.user
     queryset = Income.objects.filter(User_id = user).order_by('IncomeDate')
-    
+
     for query in queryset:
         formatted_date = query.IncomeDate.strftime("%d-%m-%Y")
         formatted_dates.append(formatted_date)
         amount = query.Amount
-        money.append(amount)
+        money.append(float(amount))
 
-    for i in range(0, len(formatted_dates)-1):
+    for i in range(len(formatted_dates)-2, 0, -1):
         if formatted_dates[i] == formatted_dates[i+1]:
             money[i] = money[i] + money[i+1]
             del money[i+1]
-    
+
     unique_formatted_dates = []
     for date in formatted_dates:
         if date not in unique_formatted_dates:
             unique_formatted_dates.append(date)
 
-    if request.method == 'POST':
+    if request.method == 'POST':            
         form = UserIncomeForm(request.POST)
         if form.is_valid():
             form.instance.User_id = request.user
@@ -45,7 +46,7 @@ def income(request):
     else:
         form = UserIncomeForm()
     context = {
-        'labels': unique_formatted_dates,
+        'labels' : unique_formatted_dates,
         'money' : money,
         'form' : form
     }
@@ -56,19 +57,21 @@ def expense(request):
     formatted_dates = []
     money = []
     user = request.user
-    queryset = Expense.objects.filter(User_id = user).order_by('ExpenseDate')
-    
+    one_week_ago = datetime.today() - timedelta(days=31)
+    #ExpenseDate__gte=one_week_ago,
+    queryset = Expense.objects.filter( User_id = user).order_by('ExpenseDate')
+
     for query in queryset:
         formatted_date = query.ExpenseDate.strftime("%d-%m-%Y")
         formatted_dates.append(formatted_date)
         amount = query.Amount
         money.append(amount)
 
-    for i in range(0, len(formatted_dates)-1):
+    for i in range(len(formatted_dates)-2, 0, -1):
         if formatted_dates[i] == formatted_dates[i+1]:
             money[i] = money[i] + money[i+1]
             del money[i+1]
-    
+
     unique_formatted_dates = []
     for date in formatted_dates:
         if date not in unique_formatted_dates:
